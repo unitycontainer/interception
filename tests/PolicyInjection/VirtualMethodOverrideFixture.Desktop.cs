d4a38cab8b66b6cb83e0554dd46a45154c7a4c94
Eugene Sadovoi <evgeni@eniks.com>
2017-10-08 19:53:12 -0400

    Refactoring

--- tests/PolicyInjection/VirtualMethodOverrideFixture.Desktop.cs
+++ tests/PolicyInjection/VirtualMethodOverrideFixture.Desktop.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Unity;
 
 namespace Microsoft.Practices.Unity.InterceptionExtension.Tests.PolicyInjection
 {

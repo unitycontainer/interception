0d031c0f0db0ae6be56ca6ec233855c2b0912261
Eugene Sadovoi <evgeni@eniks.com>
2017-11-29 11:36:57 -0500

    Create Issues.cs

---
+++ tests/Issues.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Unity.Interception.InterceptionBehaviors;
+using Unity.Interception.Interceptors.InstanceInterceptors.InterfaceInterception;
+using Unity.Interception.PolicyInjection.Pipeline;
+using Unity.Interception.ContainerIntegration;
+using Unity.Lifetime;
+using System.Diagnostics;
+
+namespace Unity.Interception.Tests
+{
+    [TestClass]
+    public class Issues
+    {
+        [TestMethod]
+        public void unitycontainer_interception_162()
+        {
+            var container = new UnityContainer();
+            container.AddNewExtension<Interception>();
+            container.RegisterType<IMyClass, MyClass>(new ContainerControlledLifetimeManager(),
+                new Interceptor<InterfaceInterceptor>(),
+                new InterceptionBehavior<LoggingInterceptionBehavior>());
+
+            var class1 = container.Resolve<IMyClass>();
+            var class2 = container.Resolve<IMyClass>();
+            class1.MyFunction();
+            class2.MyFunction();
+        }
+
+        public interface IMyClass { void MyFunction(); }
+        public class MyClass : IMyClass
+        {
+            public MyClass() { Debug.WriteLine("Create MyClass"); }
+            public void MyFunction() { Debug.WriteLine("Function of MyClass"); }
+        }
+        public class LoggingInterceptionBehavior : IInterceptionBehavior
+        {
+            public IMethodReturn Invoke(IMethodInvocation input, GetNextInterceptionBehaviorDelegate getNext)
+            {
+                Debug.WriteLine("Logging before the Function!! yeah, i like it!");
+                return getNext()(input, getNext);
+            }
+
+            public bool WillExecute => true;
+            public IEnumerable<Type> GetRequiredInterfaces() { return Type.EmptyTypes; }
+        }
+
+    }
+}

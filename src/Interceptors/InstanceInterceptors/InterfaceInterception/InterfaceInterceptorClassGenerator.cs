9ca272db1e8246f1125ccae468e9fe85ee2999b9
Eugene Sadovoi <evgeni@eniks.com>
2018-01-23 10:53:25 -0500

    Fixed #33

--- src/Interceptors/InstanceInterceptors/InterfaceInterception/InterfaceInterceptorClassGenerator.cs
+++ src/Interceptors/InstanceInterceptors/InterfaceInterception/InterfaceInterceptorClassGenerator.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.IO;
 using System.Reflection;
 using System.Reflection.Emit;
 using Unity.Interception.Interceptors.TypeInterceptors.VirtualMethodInterception.InterceptingClassGeneration;

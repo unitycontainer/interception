e2d2701aa9f0c9121bed480fa0ec300e34130c6a
Eugene Sadovoi <evgeni@eniks.com>
2018-12-08 22:03:13 -0500

    Refactoring

--- src/ContainerIntegration/ObjectBuilder/TypeInterceptionStrategy.cs
+++ src/ContainerIntegration/ObjectBuilder/TypeInterceptionStrategy.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using Unity.Build;
 using Unity.Builder;
 using Unity.Builder.Selection;
 using Unity.Builder.Strategy;
